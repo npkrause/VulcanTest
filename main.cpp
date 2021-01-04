7df1c0ab867b6628d2466fe23f9414bc83b8e468
Nicholas Krause <xerofoify@gmail.com>
2021-01-04 01:18:13 -0500

    Fix final staging bugs in Vulcan tutorial
    This fixes the final staging bugs in the Vulcan tutorail with copying
    buffers. Remember to be careful about typos!

--- main.cpp
+++ main.cpp
@@ -696,28 +696,36 @@ private:
     }
     
     void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {
-	 VkCommandBufferAllocateInfo allocInfo{};
-	 allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-	 allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-	 allocInfo.commandPool = commandPool;
-	 allocInfo.commandBufferCount = 1;
+	VkCommandBufferAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	allocInfo.commandPool = commandPool;
+	allocInfo.commandBufferCount = 1;
 
-	 VkCommandBuffer commandBuffer;
-	 vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
-	 VkBufferCopy copyRegion{};
-	 copyRegion.srcOffset = 0; // Optional
-	 copyRegion.dstOffset = 0; // Optional
-	 copyRegion.size = size;
-	 vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+	VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
 
-	 vkEndCommandBuffer(commandBuffer);
-	 VkSubmitInfo submitInfo{};
-	 submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-	 submitInfo.commandBufferCount = 1;
-	 submitInfo.pCommandBuffers = &commandBuffer;
+	VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
-	 vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
-	 vkQueueWaitIdle(graphicsQueue);
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+        VkBufferCopy copyRegion{};
+        copyRegion.size = size;
+        vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+
+        vkEndCommandBuffer(commandBuffer);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+        vkQueueWaitIdle(graphicsQueue);
+
+        vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
     }
 	 
     void createVertexBuffer() {
