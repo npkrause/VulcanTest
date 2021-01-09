0eb0ee5224e8ccb944876498c74bb3cf4ee2161d
Nicholas Krause <xerofoify@gmail.com>
2021-01-09 01:47:36 -0500

    Finish Imageviewer and Sampler
    This finishes a rough Imageviewer and sampler code. It needs to be
    tested to make sure it works after finishing the final section of
    the texture image implementation.

--- main.cpp
+++ main.cpp
@@ -145,6 +145,8 @@ private:
     VkFormat swapChainImageFormat;
     VkInstance instance;
     VkImage textureImage;
+    VkImageView textureSamplerView;
+    VkSampler textureSampler;
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
@@ -202,6 +204,8 @@ private:
         createFramebuffers();
         createCommandPool();
 	createTextureImage();
+	createTextureImageView();
+	createTextureSampler();
         createVertexBuffer();
 	createIndexBuffer();
 	createUniformBuffers();
@@ -252,6 +256,7 @@ private:
     void cleanup() {
         cleanupSwapChain();
 	
+	vkDestroyImageView(device,textureImageView, nullptr);
         vkDestroyImage(device, textureImage, nullptr);
         vkFreeMemory(device, textureImageMemory, nullptr);
 
@@ -411,6 +416,7 @@ private:
         }
 
         VkPhysicalDeviceFeatures deviceFeatures{};
+        deviceFeatures.samplerAnisotropy = VK_TRUE;
 
         VkDeviceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
@@ -493,24 +499,7 @@ private:
         swapChainImageViews.resize(swapChainImages.size());
 
         for (size_t i = 0; i < swapChainImages.size(); i++) {
-            VkImageViewCreateInfo createInfo{};
-            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-            createInfo.image = swapChainImages[i];
-            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-            createInfo.format = swapChainImageFormat;
-            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-            createInfo.subresourceRange.baseMipLevel = 0;
-            createInfo.subresourceRange.levelCount = 1;
-            createInfo.subresourceRange.baseArrayLayer = 0;
-            createInfo.subresourceRange.layerCount = 1;
-
-            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
-                throw std::runtime_error("failed to create image views!");
-            }
+	     swapChainImagesView[i] = createImageView(swapChainImages[i], VK_FORMAT_R8B8G8A8_SRGB);
         }
     }
 
@@ -758,6 +747,54 @@ private:
 	transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 	vkDestroyBuffer(device, stagingBuffer, nullptr);
         vkFreeMemory(device, stagingBufferMemory, nullptr);
+    }
+  
+    void createTextureImageView() {
+       textureImageView = createImageView(textureImage, VK_FORMAT_R8B8G8A8_SRGB);
+    }
+  
+    void createTextureSampler() {
+       VkSamplerCreateInfo samplerInfo{};
+       samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+       samplerInfo.magFilter = VK_FILTER_LINEAR;
+       samplerInfo.minFilter = VK_FILTER_LINEAR;
+       samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+       samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+       samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+       samplerInfo.anisotrophy = VK_TRUE
+       VkPhysicalDeviceProperties properties{};
+       vkGetPhysicalDeviceProperties(physicalDevice, &properties
+       samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+       samplerInfo.unnormalizedCoordinates = VK_FALSE;
+       samplerInfo.compareEnable = VK_FALSE;
+       samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+       samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+       samplerInfo.mipLodBias = 0.0f;
+       samplerInfo.minLod = 0.0f;
+       samplerInfo.maxLod = 0.0f;
+       if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create texture sampler!");
+      }
+    }  
+
+    VkImageView createImageView(VkImage image, VkFormat format) {
+       VkImageViewCreateInfo viewInfo{};
+       viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+       viewInfo.image = image;
+       viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+       viewInfo.format = format;
+       viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+       viewInfo.subresourceRange.baseMipLevel = 0;
+       viewInfo.subresourceRange.levelCount = 1;
+       viewInfo.subresourceRange.baseArrayLayer = 0;
+       viewInfo.subresourceRange.layerCount = 1;
+
+       VkImageView imageView;
+       if (vkCreateImageView(device, &viewInfo, nullptr, &imageView) != VK_SUCCESS) {
+           throw std::runtime_error("failed to create texture image view!");
+       }
+
+    return imageView;
     }
 
     void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory) {
@@ -1299,7 +1336,10 @@ private:
             swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
         }
 
-        return indices.isComplete() && extensionsSupported && swapChainAdequate;
+       VkPhysicalDeviceFeatures supportedFeatures;
+       vkGetPhysicalDeviceFeatures(device, &supportedFeatures);
+
+       return indices.isComplete() && extensionsSupported && swapChainAdequate && supportedFeatures.samplerAnisotropy;
     }
 
     bool checkDeviceExtensionSupport(VkPhysicalDevice device) {
